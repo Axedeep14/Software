1fdab6c17f5e86882d216b9899924390cd8aa508
Axedeep14 <deepakkpaliwal@gmail.com>
2019-12-05 18:06:01 +0530

    file picker thing done

--- Software/Software/MainPage.xaml.cs
+++ Software/Software/MainPage.xaml.cs
@@ -32,38 +32,30 @@ namespace Software
             editor.Text = string.Empty;
         }
 
-        protected async void OneButtonClicked(object sender, EventArgs e)
+        protected async void OnButtonClicked (object sender, EventArgs e)
         {
-            string fileName = string.Empty;
-            string filePath = string.Empty;
-
-            SemaphoreSlim signal = new SemaphoreSlim(0, 1);
-            EventHandler<FileDataReceivedArgs> handler = null;
-            handler = (s, e1) => { fileName = e1.FileName; filePath = e1.FilePath; FilePickerActivity.FilePicked -= handler; signal.Release(); };
-
             try
             {
-                // Set Allowed FileTypes here (MIME types) //
-                string[] allowedTypes = { "application/x-x509-ca-cert", "application/x-509-user-cert", "application/x-509-server-cert", "application/pkix-cert", "application/x-pkcs12" };
-                Intent mIntent = new Intent(Android.App.Application.Context, typeof(FilePickerActivity));
-                mIntent.PutExtra(FilePickerActivity.ExtraAllowedTypes, allowedTypes);
-
-                FilePickerActivity.FilePicked += handler;
-                StartActivity(mIntent);
-
-                await signal.WaitAsync();
-
-                if (string.IsNullOrWhiteSpace(filePath))
+                var crossFilePicker = Plugin.FilePicker.CrossFilePicker.Current;
+                var myResult = await crossFilePicker.PickFile();
+                if (!string.IsNullOrEmpty(myResult.FileName)) //Just the file name, it doesn't has the path
                 {
-                    return;
+                    string _fileName = myResult.FilePath;
+                    Console.WriteLine("deepak     "+_fileName);
+                    if (File.Exists(_fileName))
+                    {
+                        editor.Text = File.ReadAllText(_fileName);
+                    }
+                    /*foreach (byte b in myResult.DataArray) //Empty array
+                        b.ToString();*/
                 }
-
-                /// Do Something with FilePath /// 
             }
-            finally
+            catch (InvalidOperationException ex)
             {
-                FilePickerActivity.FilePicked -= handler;
+                ex.ToString(); //"Only one operation can be active at a time"
             }
         }
+
     }
+
 }
